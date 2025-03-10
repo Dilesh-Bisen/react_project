ecb5716557ad25bced0f75a20c41dedd1a7a4470
Dilesh Bisen <145834235+Dilesh-Bisen@users.noreply.github.com>
2025-03-10 12:25:13 +0530

    Update App.jsx

--- 01_project/Frontend/src/App.jsx
+++ 01_project/Frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Home from './Pages/Home/home';
 import NotFound from './Pages/Not_Found/not_found';
 import Success from './Pages/Success/success';
-import './App.css'
+import './App.css';
+
 const App = () => {
     return (
         <>
@@ -16,8 +17,11 @@ const App = () => {
                 </Routes>
                 <Toaster />
             </Router>
+            <footer>
+                <p>Visit our website: <a href="https://restaurant-book-table.vercel.app/" target="_blank" rel="noopener noreferrer">Restaurant Book Table</a></p>
+            </footer>
         </>
-    )
-}
+    );
+};
 
-export default App
+export default App;
