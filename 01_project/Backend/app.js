7af2db78c2e50c946ea65c3850454ea766e770ca
Dilesh Bisen <145834235+Dilesh-Bisen@users.noreply.github.com>
2025-03-10 12:55:48 +0530

    Update app.js

--- 01_project/Backend/app.js
+++ 01_project/Backend/app.js
@@ -5,20 +5,19 @@ import { db_connection } from "./database/db_connection.js";
 import { error_middleware } from "./error/error.js";
 import reservation_router from "./routes/reservation_route.js";
 
-dotenv.config({ path: "./config/config.env" }); 
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
 app.use(
-    cors({
-        origin: [process.env.FRONTEND_URL],
-        methods: ["POST", "OPTIONS"],
-        credentials: true,
-    })
+  cors({
+    origin: "https://react-project-ten-kohl.vercel.app", // Your frontend URL
+    methods: ["POST", "OPTIONS"],
+    credentials: true,
+  })
 );
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 db_connection();
@@ -28,10 +27,10 @@ app.use(error_middleware);
 app.use("/api/v1/reservation", reservation_router);
 
 app.get("/", (_req, res, _next) => {
-    return res.status(200).json({
-        success: true,
-        message: "HELLO WORLD AGAIN"
-    })
-})
+  return res.status(200).json({
+    success: true,
+    message: "HELLO WORLD AGAIN",
+  });
+});
 
 export default app;
