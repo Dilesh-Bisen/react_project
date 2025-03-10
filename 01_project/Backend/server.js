ef9c64893978b512a4360d80cb0ee646484eafaa
Dilesh Bisen <145834235+Dilesh-Bisen@users.noreply.github.com>
2025-03-10 12:56:06 +0530

    Update server.js

--- 01_project/Backend/server.js
+++ 01_project/Backend/server.js
@@ -2,6 +2,6 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    console.log('Frontend URL: https://react-project-ten-kohl.vercel.app/');
+  console.log(`Server running on port ${PORT}`);
+  console.log("Frontend URL: https://react-project-ten-kohl.vercel.app/");
 });
