f790bcffb850860350e28b73db329828eef1de34
Dilesh Bisen <145834235+Dilesh-Bisen@users.noreply.github.com>
2025-03-10 13:07:48 +0530

    Update home.jsx

--- 01_project/Frontend/src/Pages/Home/home.jsx
+++ 01_project/Frontend/src/Pages/Home/home.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import HeroSection from '../../components/hero_section'
-import About from '../../components/about'
-import Qualities from '../../components/qualities'
-import Menu from '../../components/menu'
-import WhoAreWe from '../../components/who_are_we'
-import Team from '../../components/team'
-import Reservation from '../../components/reservation'
-import Footer from '../../components/footer'
+import React from "react";
+import HeroSection from "../../components/hero_section";
+import About from "../../components/about";
+import Qualities from "../../components/qualities";
+import Menu from "../../components/menu";
+import WhoAreWe from "../../components/who_are_we";
+import Team from "../../components/team";
+import Reservation from "../../components/reservation"; // Update this component
+import Footer from "../../components/footer";
 
 const Home = () => {
-    return (
-        <>
-            <HeroSection />
-            <About />
-            <Qualities />
-            <Menu />
-            <Reservation />
-            <WhoAreWe />
-            <Team />
-            <Footer /> 
-        </>
-    )
-}
+  return (
+    <>
+      <HeroSection />
+      <About />
+      <Qualities />
+      <Menu />
+      <Reservation
+        apiUrl="https://restaurant-server-sable.vercel.app/api/v1/reservation" // Pass the backend URL as a prop
+      />
+      <WhoAreWe />
+      <Team />
+      <Footer />
+    </>
+  );
+};
 
-export default Home
+export default Home;
